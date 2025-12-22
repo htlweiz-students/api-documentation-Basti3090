8ba08e4d938a6c244ed12d8811c246b705bf1537
Student <foobar@sr.htlweiz.at>
2025-12-22 10:50:27 +0100

    Done

---
+++ AverageCalculator/Class1.cs
@@ -0,0 +1,31 @@
+﻿namespace AverageCalculator;
+
+public class AverageCalculator
+{
+    private List<double> values;
+
+
+    public AverageCalculator()
+    {
+        values = new List<double>();
+    }
+
+    public void add(double value)
+    {
+        values.Add(value);
+    }
+    public void add(double[] values)
+    {
+        if (values == null) return;
+
+
+        foreach (var v in values)
+        {
+            this.values.Add(v);
+        }
+    }
+
+    public int count() {
+        return values.Count;
+    }
+}
---
+++ Project/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using AverageCalculator;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        AverageCalculator.AverageCalculator calc =
+            new AverageCalculator.AverageCalculator();
+
+        calc.add(10);
+        calc.add(20.5);
+
+        double[] values = { 5, 15, 25 };
+        calc.add(values);
+
+        Console.WriteLine(calc.count());
+    }
+}
